aaaecfbe919cab3f917348553862e3d97ab3ddb5
Stubbs <stubbs0x@protonmail.com>
2024-06-02 05:07:43 -0600

    added harpoon, lualine, obsidian, and pomo

---
+++ lua/custom/plugins/lualine.lua
@@ -0,0 +1,36 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  lazy = true,
+  cmd = { 'TimerStart', 'TimerRepeat' },
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'rcarriga/nvim-notify',
+  },
+  config = function()
+    require('lualine').setup {
+      options = {
+        theme = 'gruvbox_dark',
+      },
+      sections = {
+        lualine_x = {
+          function()
+            local ok, pomo = pcall(require, 'pomo')
+            if not ok then
+              return ''
+            end
+
+            local timer = pomo.get_first_to_finish()
+            if timer == nil then
+              return ''
+            end
+
+            return '󰄉 ' .. tostring(timer)
+          end,
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
+      },
+    }
+  end,
+}
