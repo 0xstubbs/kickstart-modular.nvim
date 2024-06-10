711d71cf1c043a9d545b1bbbdc77c195e3687f23
Stubbs <stubbs0x@protonmail.com>
2024-06-10 07:57:40 -0600

    added crates.lua, gruvbox.lua, lspconfig.lua, nvim-comp.lua, telescope.lua, todo-comments.lua, treesitter.lua, and yanky.lua to /lua/custom/plugins/

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-lualine/lualine.nvim',
-  lazy = true,
+  lazy = false,
   cmd = { 'TimerStart', 'TimerRepeat' },
   dependencies = {
     'nvim-tree/nvim-web-devicons',
--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,15 +1,20 @@
+vim.opt.conceallevel = 2
 return {
   'epwalsh/obsidian.nvim',
   version = '*', -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = 'markdown',
+
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
+  --    'BufReadPre '
+  -- .. vim.fn.expand '~'
+  --  .. '~/Documents/stubbs/**.md',
+  --   'BufReadPre ~/Documents/stubbs/**.md',
+  --    'BufNewFile ~/Documents/stubbs/**.md',
+  --},
+  --},
   dependencies = {
     -- Required.
     'nvim-lua/plenary.nvim',
@@ -19,15 +24,15 @@ return {
   opts = {
     workspaces = {
       {
-        name = 'personal',
-        path = '~/vaults/personal',
-      },
-      {
-        name = 'work',
-        path = '~/vaults/work',
+        name = 'stubbs',
+        path = '~/Documents/stubbs',
       },
     },
 
-    -- see below for full list of options 👇
+    --      see below for full list of options 👇
+    --       daily_notes = {
+    --         folder = 'Periodic Notes/Daily Notes',
+    --         template = 'Template_daily',
+    --       },
   },
 }
